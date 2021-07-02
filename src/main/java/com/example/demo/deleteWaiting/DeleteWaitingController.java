4ddec793a08d2d87cc59753691f1251093d6ea6a
Minho Yun <minhoyun.dev@gmail.com>
2021-07-02 15:45:08 +0900

    Git test

--- src/main/java/com/example/demo/deleteWaiting/DeleteWaitingController.java
+++ src/main/java/com/example/demo/deleteWaiting/DeleteWaitingController.java
@@ -1,16 +1,16 @@
-package com.example.demo.queue;
+package com.example.demo.deleteWaiting;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//　待ち状態管理
+//　待ち状態解除
 @Controller
-public class QueueController {
+public class DeleteWaitingController {
 
-	@GetMapping(value="/queue")
-	public String Get_Queue(Model model) {
+	@GetMapping(value="/deleteWaiting")
+	public String Get_DeleteWaiting(Model model) {
 //		model.addAttribute("loginForm", new LoginForm());
-		return "queue";
+		return "deleteWaiting";
 	}
 }
