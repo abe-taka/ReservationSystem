60f0e38d3e9da88bc06932e5ba907c7e5b7c532f
Minho Yun <minhoyun.dev@gmail.com>
2021-07-26 16:22:27 +0900

    Update front

--- src/main/java/com/example/demo/repositories/SoftRepository.java
+++ src/main/java/com/example/demo/repositories/SoftRepository.java
@@ -12,6 +12,6 @@ import com.example.demo.entities.SoftEntity;
 public interface SoftRepository extends JpaRepository<SoftEntity,String>{
 
 	// ソフトコード取得(指定した階のマシーンのソフト)
-	@Query(value = "SELECT z.m07_soft_code,z.m07_maker,z.m07_soft_name FROM m06_machine x,m08_machine_soft y,m07_soft z WHERE x.m06_floor = :floor AND x.m06_machine_code = y.m08_machine_code AND y.m08_soft_code = z.m07_soft_code", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT z.m07_soft_code,z.m07_maker,z.m07_soft_name FROM m06_machine x,m08_machine_soft y,m07_soft z WHERE x.m06_floor = :floor AND x.m06_machine_code = y.m08_machine_code AND y.m08_soft_code = z.m07_soft_code", nativeQuery = true)
 	public List<SoftEntity> findBySoftid(@Param("floor") int floor);
 }
