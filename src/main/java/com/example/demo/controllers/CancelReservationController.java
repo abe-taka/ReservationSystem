abeff1324490195a2180b7dc89773fba977b515c
Minho Yun <minhoyun.dev@gmail.com>
2021-08-04 17:19:55 +0900

    Change design

--- src/main/java/com/example/demo/controllers/CancelReservationController.java
+++ src/main/java/com/example/demo/controllers/CancelReservationController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 //　予約取消
 @Controller
 public class CancelReservationController {
+	
+	//@Autowired
+	//SeatStatusRepository seatStatusRepository;
+	
+	// セッションデータ用
+	String session_data = null;
 	
 	@GetMapping(value="/cancelReservation")
 	public String Get_CancelReservation(Model model) {
